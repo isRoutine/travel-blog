36e4de798a52ba51ccfad1db109b3f2f7840ab3f
isRoutine <gianluca.farinaccio@gmail.com>
2022-05-24 15:56:16 +0200

    Spring project creation

---
+++ travel-blog/src/main/java/it/uniroma3/travelblog/TravelBlogApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.travelblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelBlogApplication.class, args);
+	}
+
+}
---
+++ travel-blog/src/test/java/it/uniroma3/travelblog/TravelBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.travelblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
