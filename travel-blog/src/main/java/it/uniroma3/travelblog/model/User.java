a116a42b2666a433ff6dc6cd50b929065b78529d
simone_albero <simone.albero29@gmail.com>
2022-05-27 13:11:52 +0200

    DomainModel Update
    modificati alcuni dettagli, aggiunta classe Image e metodi getter e setter

--- travel-blog/src/main/java/it/uniroma3/travelblog/model/User.java
+++ travel-blog/src/main/java/it/uniroma3/travelblog/model/User.java
@@ -1,6 +1,7 @@
 package it.uniroma3.travelblog.model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -35,6 +36,7 @@ public class User {
 	
 	@NotBlank
 	@NotNull
+	@UniqueElements
 	private String userName;
 	
 	@NotNull
@@ -46,14 +48,64 @@ public class User {
 	private List<Experience> experiences;
 	
 	
-	/*todo pwd*/
-	
-	public long getId() {
+	public User() {
+		this.experiences = new ArrayList<Experience>();
+	}
+
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Experience> getExperiences() {
+		return experiences;
+	}
+
+	public void setExperiences(List<Experience> experiences) {
+		this.experiences = experiences;
 	}
 	
 }
